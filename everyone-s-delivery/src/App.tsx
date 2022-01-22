f1e0deb986eca185968e708d1107d28d923b68c3
hyeon <entropy1760@gmail.com>
2022-01-23 03:39:27 +0900

    회원가입 페이지 추가

--- everyone-s-delivery/src/App.tsx
+++ everyone-s-delivery/src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import "./App.css";
 import Footer from "./components/Footer";
+import SignupScreen from "./screens/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginScreen />}></Route>
+        <Route path="/signup" element={<SignupScreen />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
